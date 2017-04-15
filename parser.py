1a4a3e7808d07214c0683f28ffdb1620eee6946a
Louis Warren <louisswarren@users.noreply.github.com>
2017-04-15 16:52:28 +1200

    Update parser.py

--- parser.py
+++ parser.py
@@ -2,50 +2,89 @@ from scanner import scanner
 from token import Token
 
 class ParserError(Exception):
-    def __init__(self, token, *expected_tokens):
+    def __init__(self, token_stream, *expected_tokens):
         template = "Received {}, expected {{{}}}"
+        token = token_stream.current
         self.message = template.format(token, ', '.join(expected_tokens))
 
     def __str__(self):
         return self.message
 
+class TokenStream:
+    """Stream of tokens, with features needed by a parser."""
 
-def consume(tokcar, tokcdr, *expected_tokens):
-    """Consume a given token, checking that it is valid. Returns the next
-    token, and the literal representation of the given token. Note that tokcdr
-    gets modified.
-    Example usage:
-        tokcar, _ = consume(tokcar, tokcdr, Token.ADD, Token.MUL)"""
-    if tokcar[0] not in expected_tokens:
-        raise ParserError(tokcar, *expected_tokens)
-    print("Consuming", tokcar)
-    return next(tokcdr), tokcar[1]
+    def __init__(self, source):
+        self.source = source
+        self.current = next(self.source)
 
+    def __iter__(self):
+        yield self.current
+        yield from self.source
+
+    def lookahead(self):
+        """See the token type at the front of the stream"""
+        if self.current:
+            return self.current[0]
+
+    def consume(self, *expected):
+        """Consume the current token, return the next token's literal
+        representation and type"""
+        print("Consuming,", self.current)
+        if self.lookahead() not in expected:
+            raise ParserError(self.current, expected)
+        consumed_type, consumed_literal = self.current
+        try:
+            self.current = next(self.source)
+        except StopIteration:
+            self.current = None
+        return consumed_literal, consumed_type
 
 def parser(tokens):
-    statements = []
-    for token in tokens:
-        statements.append(statement(token, tokens))
+    stream = TokenStream(tokens)
+    return statements(stream)
 
 
-def statement(tokcar, tokcdr):
-    if tokcar[0] == Token.LET:
-        return let(tokcar, tokcdr)
+class statements_ast:
+    def __init__(self, statements):
+        self.statements = statements
+
+    def tree(self, indent=0):
+        return '\n'.join(st.tree(indent+1) for st in self.statements)
+
+def statements(stream):
+    statements_list = []
+    while stream.lookahead():
+        statements_list.append(statement(stream))
+    return statements_ast(statements_list)
+
+def statement(stream):
+    if stream.lookahead() == Token.LET:
+        return let(stream)
     else:
-        raise ParserError(tokcar[0], Token.LET)
+        raise ParserError(stream, Token.LET)
 
-def let(tokcar, tokcdr):
-    tokcar, _ = consume(tokcar, tokcdr, Token.LET)
-    tokcar, var = consume(tokcar, tokcdr, Token.ID)
-    if tokcar == Token.LSBRA:
-        tokcar, _ = consume(tokcar, tokcdr, Token.LSBRA)
-        tokcar, size_tok = consume(tokcar, tokcdr, Token.NUM)
-        tokcar, _ = consume(tokcar, tokcdr, Token.RSBRA)
+class let_ast:
+    def __init__(self, var, size):
+        self.var = var
+        self.size = size
+
+    def tree(self, indent=0):
+        return '\t' * indent + f'Let {self.var}[{self.size}]'
+
+def let(stream):
+    stream.consume(Token.LET)
+    var, _ = stream.consume(Token.ID)
+    if stream.lookahead() == Token.LSBRA:
+        stream.consume(Token.LSBRA)
+        size_tok, _ = stream.consume(Token.NUM)
+        stream.consume(Token.RSBRA)
         size = int(size_tok)
     else:
         size = 1
+    return let_ast(var, size)
 
 
 with open('parsetest.mc') as f:
-    parser(scanner(f.read(), Token.regexes))
+    ast = parser(scanner(f.read(), Token.regexes))
+    print(ast.tree())
 
